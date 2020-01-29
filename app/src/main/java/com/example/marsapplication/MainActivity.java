ed66fb49df4d961ec06e6dd756a5dd46642b38ef
YokoBurke <fujikurock@gmail.com>
2020-01-28 21:06:35 -0500

    Implemented ButterKnife Library

--- app/src/main/java/com/example/marsapplication/MainActivity.java
+++ app/src/main/java/com/example/marsapplication/MainActivity.java
@@ -3,12 +3,57 @@ package com.example.marsapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.material.button.MaterialButton;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.curiosity_button)
+    MaterialButton buttonCuriosity;
+
+    @BindView(R.id.opportunity_button)
+    MaterialButton buttonOpportunity;
+
+    @BindView(R.id.spirit_button)
+    MaterialButton buttonSpirit;
+
+    @BindView(R.id.text_rover_description)
+    TextView roverDescTextView;
+
+    @BindView(R.id.text_rover_title)
+    TextView roverTitleTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
     }
+
+    @OnClick(R.id.curiosity_button)
+    public void buttonCuriosityClicked() {
+        roverDescTextView.setText(getString(R.string.mars_curiosity));
+        roverTitleTextView.setText("Curiosity");
+    }
+
+    @OnClick(R.id.spirit_button)
+    public void buttonSpiritClicked() {
+        roverDescTextView.setText(getString(R.string.mars_spirit));
+        roverTitleTextView.setText("Spirit");
+    }
+
+    @OnClick(R.id.opportunity_button)
+    public void buttonOnpportunityClicked() {
+        roverDescTextView.setText(getString(R.string.mars_opportunity));
+        roverTitleTextView.setText("Opportunity");
+    }
+
 }
